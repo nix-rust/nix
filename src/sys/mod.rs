19f3690627c45ab91ca849d1cb0c9781716c4367
Valerii Hiora <valerii.hiora@gmail.com>
2015-02-13 14:49:52 +0200

    iOS: term support is really redundant

--- src/sys/mod.rs
+++ src/sys/mod.rs
@@ -8,6 +8,7 @@ pub mod event;
 #[cfg(target_os = "linux")]
 pub mod eventfd;
 
+#[cfg(not(target_os = "ios"))]
 pub mod ioctl;
 
 pub mod signal;
@@ -16,6 +17,7 @@ pub mod socket;
 
 pub mod stat;
 
+#[cfg(not(target_os = "ios"))]
 pub mod termios;
 
 #[cfg(target_os = "linux")]
