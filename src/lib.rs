512aa98dc6c8167a666be13955e6a4614c41ebbc
Carl Lerche <me@carllerche.com>
2015-02-14 21:20:39 -0800

    Fix warnings

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![crate_name = "nix"]
 
-#![feature(collections, core, linkage, libc, hash, os)]
+#![feature(collections, core, linkage, libc, hash, os, std_misc)]
 #![allow(non_camel_case_types)]
 
 #[macro_use]
--- tests/tests.rs
+++ tests/tests.rs
@@ -1,4 +1,3 @@
-#![feature(core)]
 extern crate nix;
 
 mod sys {
