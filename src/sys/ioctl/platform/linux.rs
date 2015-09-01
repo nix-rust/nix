c6909d66630474b0e7d6a66b8bf9e880f6422e8a
Paul Osborne <osbpau@gmail.com>
2015-09-01 01:24:45 -0500

    ioctl: clean up source file for ioctls
    Readability was unecessarily impaired via a myriad of attributes to
    hide constants from the documentation.  If these attributes are exposed
    publically, including them in the documentation makes sense.

--- src/sys/ioctl/platform/linux.rs
+++ src/sys/ioctl/platform/linux.rs
@@ -1,59 +1,46 @@
-#[doc(hidden)]
 pub const NRBITS: u32 = 8;
-#[doc(hidden)]
 pub const TYPEBITS: u32 = 8;
 
 #[cfg(any(target_arch = "mips", target_arch = "powerpc"))]
 mod consts {
-    #[doc(hidden)]
     pub const NONE: u8 = 1;
-    #[doc(hidden)]
     pub const READ: u8 = 2;
-    #[doc(hidden)]
     pub const WRITE: u8 = 4;
-    #[doc(hidden)]
     pub const SIZEBITS: u8 = 13;
-    #[doc(hidden)]
     pub const DIRBITS: u8 = 3;
 }
 
-#[cfg(not(any(target_arch = "powerpc", target_arch = "mips", target_arch = "x86", target_arch = "arm", target_arch = "x86_64", target_arch = "aarch64")))]
+#[cfg(not(any(target_arch = "powerpc",
+              target_arch = "mips",
+              target_arch = "x86",
+              target_arch = "arm",
+              target_arch = "x86_64",
+              target_arch = "aarch64")))]
 use this_arch_not_supported;
 
 // "Generic" ioctl protocol
-#[cfg(any(target_arch = "x86", target_arch = "arm", target_arch = "x86_64", target_arch = "aarch64"))]
+#[cfg(any(target_arch = "x86",
+          target_arch = "arm",
+          target_arch = "x86_64",
+          target_arch = "aarch64"))]
 mod consts {
-    #[doc(hidden)]
     pub const NONE: u8 = 0;
-    #[doc(hidden)]
     pub const READ: u8 = 2;
-    #[doc(hidden)]
     pub const WRITE: u8 = 1;
-    #[doc(hidden)]
     pub const SIZEBITS: u8 = 14;
-    #[doc(hidden)]
     pub const DIRBITS: u8 = 2;
 }
 
-#[doc(hidden)]
 pub use self::consts::*;
 
-#[doc(hidden)]
 pub const NRSHIFT: u32 = 0;
-#[doc(hidden)]
 pub const TYPESHIFT: u32 = NRSHIFT + NRBITS as u32;
-#[doc(hidden)]
 pub const SIZESHIFT: u32 = TYPESHIFT + TYPEBITS as u32;
-#[doc(hidden)]
 pub const DIRSHIFT: u32 = SIZESHIFT + SIZEBITS as u32;
 
-#[doc(hidden)]
 pub const NRMASK: u32 = (1 << NRBITS) - 1;
-#[doc(hidden)]
 pub const TYPEMASK: u32 = (1 << TYPEBITS) - 1;
-#[doc(hidden)]
 pub const SIZEMASK: u32 = (1 << SIZEBITS) - 1;
-#[doc(hidden)]
 pub const DIRMASK: u32 = (1 << DIRBITS) - 1;
 
 /// Encode an ioctl command.
@@ -190,11 +177,7 @@ pub fn ioc_size(nr: u32) -> u32 {
     ((nr >> SIZESHIFT) as u32) & SIZEMASK
 }
 
-#[doc(hidden)]
 pub const IN: u32 = (WRITE as u32) << DIRSHIFT;
-#[doc(hidden)]
 pub const OUT: u32 = (READ as u32) << DIRSHIFT;
-#[doc(hidden)]
 pub const INOUT: u32 = ((READ|WRITE) as u32) << DIRSHIFT;
-#[doc(hidden)]
 pub const SIZE_MASK: u32 = SIZEMASK << SIZESHIFT;
