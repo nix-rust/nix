8ca6d821e466b38721c5c7e8c9ef6d2758bc9504
Nikolay Amiantov <ab@fmap.me>
2016-02-19 17:22:50 +0300

    mqueue: change types to allow more cases

--- test/test_mq.rs
+++ test/test_mq.rs
@@ -20,8 +20,8 @@ fn test_mq_send_and_receive() {
     const MSG_SIZE: c_long =  32;
     let attr =  MqAttr::new(0, 10, MSG_SIZE, 0);
     let mq_name_in_parent = &CString::new(b"/a_nix_test_queue".as_ref()).unwrap();
-    let mqd_in_parent = mq_open(mq_name_in_parent, O_CREAT | O_WRONLY, S_IWUSR | S_IRUSR | S_IRGRP | S_IROTH, &attr).unwrap();
-    let msg_to_send = &CString::new("msg_1").unwrap();
+    let mqd_in_parent = mq_open(mq_name_in_parent, O_CREAT | O_WRONLY, S_IWUSR | S_IRUSR | S_IRGRP | S_IROTH, Some(&attr)).unwrap();
+    let msg_to_send = "msg_1".as_bytes();
 
     mq_send(mqd_in_parent, msg_to_send, 1).unwrap();
 
@@ -31,7 +31,7 @@ fn test_mq_send_and_receive() {
     match pid {
         Ok(Child) => {
             let mq_name_in_child =  &CString::new(b"/a_nix_test_queue".as_ref()).unwrap();
-            let mqd_in_child = mq_open(mq_name_in_child, O_CREAT | O_RDONLY, S_IWUSR | S_IRUSR | S_IRGRP | S_IROTH, &attr).unwrap();
+            let mqd_in_child = mq_open(mq_name_in_child, O_CREAT | O_RDONLY, S_IWUSR | S_IRUSR | S_IRGRP | S_IROTH, Some(&attr)).unwrap();
             let mut buf = [0u8; 32];
             mq_receive(mqd_in_child, &mut buf, 1).unwrap();
             write(writer, &buf).unwrap();  // pipe result to parent process. Otherwise cargo does not report test failures correctly
@@ -59,7 +59,7 @@ fn test_mq_getattr() {
     const MSG_SIZE: c_long =  32;
     let initial_attr =  MqAttr::new(0, 10, MSG_SIZE, 0);
     let mq_name = &CString::new("/attr_test_get_attr".as_bytes().as_ref()).unwrap();
-    let mqd = mq_open(mq_name, O_CREAT | O_WRONLY, S_IWUSR | S_IRUSR | S_IRGRP | S_IROTH, &initial_attr).unwrap();
+    let mqd = mq_open(mq_name, O_CREAT | O_WRONLY, S_IWUSR | S_IRUSR | S_IRGRP | S_IROTH, Some(&initial_attr)).unwrap();
     let read_attr = mq_getattr(mqd);
     assert!(read_attr.unwrap() == initial_attr);
     mq_close(mqd).unwrap();
@@ -70,7 +70,7 @@ fn test_mq_setattr() {
     const MSG_SIZE: c_long =  32;
     let initial_attr =  MqAttr::new(0, 10, MSG_SIZE, 0);
     let mq_name = &CString::new("/attr_test_get_attr".as_bytes().as_ref()).unwrap();
-    let mqd = mq_open(mq_name, O_CREAT | O_WRONLY, S_IWUSR | S_IRUSR | S_IRGRP | S_IROTH, &initial_attr).unwrap();
+    let mqd = mq_open(mq_name, O_CREAT | O_WRONLY, S_IWUSR | S_IRUSR | S_IRGRP | S_IROTH, Some(&initial_attr)).unwrap();
 
     let new_attr =  MqAttr::new(0, 20, MSG_SIZE * 2, 100);
     let old_attr = mq_setattr(mqd, &new_attr);
@@ -96,7 +96,7 @@ fn test_mq_set_nonblocking() {
     const MSG_SIZE: c_long =  32;
     let initial_attr =  MqAttr::new(0, 10, MSG_SIZE, 0);
     let mq_name = &CString::new("/attr_test_get_attr".as_bytes().as_ref()).unwrap();
-    let mqd = mq_open(mq_name, O_CREAT | O_WRONLY, S_IWUSR | S_IRUSR | S_IRGRP | S_IROTH, &initial_attr).unwrap();
+    let mqd = mq_open(mq_name, O_CREAT | O_WRONLY, S_IWUSR | S_IRUSR | S_IRGRP | S_IROTH, Some(&initial_attr)).unwrap();
     mq_set_nonblock(mqd).unwrap();
     let new_attr = mq_getattr(mqd);
     assert!(new_attr.unwrap().mq_flags == O_NONBLOCK.bits() as c_long);
@@ -112,7 +112,7 @@ fn test_mq_unlink() {
     let initial_attr =  MqAttr::new(0, 10, MSG_SIZE, 0);
     let mq_name_opened = &CString::new("/mq_unlink_test".as_bytes().as_ref()).unwrap();
     let mq_name_not_opened = &CString::new("/mq_unlink_test".as_bytes().as_ref()).unwrap();
-    let mqd = mq_open(mq_name_opened, O_CREAT | O_WRONLY, S_IWUSR | S_IRUSR | S_IRGRP | S_IROTH, &initial_attr).unwrap();
+    let mqd = mq_open(mq_name_opened, O_CREAT | O_WRONLY, S_IWUSR | S_IRUSR | S_IRGRP | S_IROTH, Some(&initial_attr)).unwrap();
 
     let res_unlink = mq_unlink(mq_name_opened);
     assert!(res_unlink == Ok(()) );
