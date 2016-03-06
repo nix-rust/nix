b8f0535127bd1d2b2750a9ffd31d8da9710465a2
Paul Osborne <osbpau@gmail.com>
2016-03-05 18:01:09 -0600

    test: clean up the test build script
    Signed-off-by: Paul Osborne <osbpau@gmail.com>

--- nix-test/build.rs
+++ nix-test/build.rs
@@ -13,46 +13,9 @@ pub fn main() {
         "UNKNOWN"
     };
 
-    let mut config = gcc::Config::new();
-
-    for file in &["src/const.c", "src/sizes.c"] {
-        config.file(file);
-    }
-
-    config.define(os, None);
-
-    config.compile("libnixtest.a");
+    gcc::Config::new()
+        .file("src/const.c")
+        .file("src/sizes.c")
+        .define(os, None)
+        .compile("libnixtest.a");
 }
-
-/*
-use std::env;
-use std::process::Command;
-
-pub fn main() {
-    let root   = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let make   = root.clone() + "/Makefile";
-    let src    = root.clone() + "/src";
-    let out    = env::var("OUT_DIR").unwrap();
-    let target = env::var("TARGET").unwrap();
-
-    let os = if target.contains("linux") {
-        "LINUX"
-    } else if target.contains("darwin") {
-        "DARWIN"
-    } else {
-        "UNKNOWN"
-    };
-
-    let res = Command::new("make")
-        .arg("-f").arg(&make)
-        .current_dir(&out)
-        .env("VPATH", &src)
-        .env("OS", os)
-        .spawn().unwrap()
-        .wait().unwrap();
-
-    assert!(res.success());
-
-    println!("cargo:rustc-flags=-L {}/", out);
-}
-*/
