cd3fdaf9918f5f14b9f818f5f5b616446f28260b
Paul Osborne <osbpau@gmail.com>
2016-03-13 15:04:46 -0500

    testing: move to new posborne/rust-cross images
    The rust-cross image is no longer all-in-one but broken out by architecture
    class (or platform in the case of android).
    
    Signed-off-by: Paul Osborne <osbpau@gmail.com>

--- ci/run-all.sh
+++ ci/run-all.sh
@@ -6,49 +6,23 @@
 
 set -e
 
-RUST_VERSIONS=${RUST_VERSIONS:-"\
-    1.6.0 \
-    1.7.0 \
-    beta \
-    nightly"}
-
-# Disabled (not working presently) but with some support in the target
-# image:
-# - i686-apple-darwin
-# - x86_64-apple-darwin
-
-RUST_TARGETS=${RUST_TARGETS:-"\
-    aarch64-unknown-linux-gnu \
-    arm-linux-androideabi \
-    arm-unknown-linux-gnueabi \
-    arm-unknown-linux-gnueabihf \
-    i686-unknown-linux-gnu \
-    mips-unknown-linux-gnu \
-    mipsel-unknown-linux-gnu \
-    x86_64-unknown-linux-gnu \
-    x86_64-unknown-linux-musl"}
-
-DOCKER_IMAGE=${DOCKER_IMAGE:-"posborne/rust-cross"}
-
 BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+RUN_DOCKER="${BASE_DIR}/ci/run-docker.sh"
 
-test_nix() {
-    version=$1
-    target=$2
+export RUST_VERSION=1.7.0
 
-    docker run -i -t \
-           -v ${BASE_DIR}:/source \
-           -e CARGO_TARGET_DIR=/build \
-           ${DOCKER_IMAGE} \
-           /source/ci/run.sh ${version} ${target}
-}
+export DOCKER_IMAGE=posborne/rust-cross:base
+RUST_TARGET=x86_64-unknown-linux-gnu ${RUN_DOCKER}
+RUST_TARGET=x86_64-unknown-linux-musl ${RUN_DOCKER}
 
-# Ensure up to date (short compared to everything else)
-docker pull ${DOCKER_IMAGE}
+export DOCKER_IMAGE=posborne/rust-cross:arm
+RUST_TARGET=aarch64-unknown-linux-gnu ${RUN_DOCKER}
+RUST_TARGET=arm-linux-gnueabi test_nix ${RUN_DOCKER}
+RUST_TARGET=arm-linux-gnueabihf test_nix ${RUN_DOCKER}
 
-# Run tests for each version/target combination
-for version in ${RUST_VERSIONS}; do
-    for target in ${RUST_TARGETS}; do
-        test_nix $version $target || true
-    done
-done
+export DOCKER_IMAGE=posborne/rust-cross:mips
+RUST_TARGET=mips-unknown-linux-gnu test_nix ${RUN_DOCKER}
+RUST_TARGET=mipsel-unknown-linux-gnu test_nix ${RUN_DOCKER}
+
+export DOCKER_IMAGE=posborne/rust-cross:android ${RUN_DOCKER}
+RUST_TARGET=arm-linux-androideabi test_nix ${RUN_DOCKER}
