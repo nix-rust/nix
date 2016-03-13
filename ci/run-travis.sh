6ad81d1ce34979253395aa39918899b3134a09c0
Paul Osborne <osbpau@gmail.com>
2016-03-13 15:16:03 -0500

    testing: first shot at testing with travis for arm, mips, android, ...
    This is based on libc and the new docker-based testing infrastructure
    via posborne/rust-cross.
    
    Signed-off-by: Paul Osborne <osbpau@gmail.com>

---
+++ ci/run-travis.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# Entry point for all travis builds, this will set up the Travis environment by
+# downloading any dependencies. It will then execute the `run.sh` script to
+# build and execute all tests.
+#
+# Much of this script was liberally stolen from rust-lang/libc
+#
+# Key variables that may be set from Travis:
+# - TRAVIS_RUST_VERSION: 1.1.0 ... stable/nightly/beta
+# - TRAVIS_OS_NAME: linux/osx
+# - DOCKER_IMAGE: posborne/rust-cross:arm
+# - TARGET: e.g. arm-unknown-linux-gnueabihf
+
+set -ex
+
+BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+
+if [ "$TRAVIS_OS_NAME" = "linux" ]; then
+  OS=unknown-linux-gnu
+else
+  OS=apple-darwin
+fi
+
+export HOST=$ARCH-$OS
+if [ "$TARGET" = "" ]; then
+  TARGET=$HOST
+fi
+
+if [ "$DOCKER_IMAGE" = "" ]; then
+  RUST_TEST_THREADS=1 cargo test
+else
+  export RUST_VERSION=${TRAVIS_RUST_VERSION}
+  export RUST_TARGET=${TARGET}
+  export DOCKER_IMAGE=${DOCKER_IMAGE}
+  ${BASE_DIR}/ci/run-docker.sh
+fi
