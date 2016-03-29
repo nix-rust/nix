eb00cd62f1b0aa2633b754b1016c095f4fda29d4
Kamal Marhubi <kamal@marhubi.com>
2016-03-29 15:46:48 -0400

    tests: Unwrap result of fork in tests
    All tests were panicking in the `Err(..)` case. This makes the code a
    little cleaner by moving the panic into a call to `expect()`, tidying up
    the match arms.

--- test/sys/test_wait.rs
+++ test/sys/test_wait.rs
@@ -6,25 +6,21 @@ use libc::exit;
 
 #[test]
 fn test_wait_signal() {
-    match fork() {
-      Ok(Child) => loop { /* Wait for signal */ },
-      Ok(Parent(child_pid)) => {
+    match fork().expect("fork") {
+      Child => loop { /* Wait for signal */ },
+      Parent(child_pid) => {
           kill(child_pid, SIGKILL).ok().expect("Error: Kill Failed");
           assert_eq!(waitpid(child_pid, None), Ok(WaitStatus::Signaled(child_pid, SIGKILL, false)));
       },
-      // panic, fork should never fail unless there is a serious problem with the OS
-      Err(_) => panic!("Error: Fork Failed")
     }
 }
 
 #[test]
 fn test_wait_exit() {
-    match fork() {
-      Ok(Child) => unsafe { exit(12); },
-      Ok(Parent(child_pid)) => {
+    match fork().expect("fork") {
+      Child => unsafe { exit(12); },
+      Parent(child_pid) => {
           assert_eq!(waitpid(child_pid, None), Ok(WaitStatus::Exited(child_pid, 12)));
       },
-      // panic, fork should never fail unless there is a serious problem with the OS
-      Err(_) => panic!("Error: Fork Failed")
     }
 }
