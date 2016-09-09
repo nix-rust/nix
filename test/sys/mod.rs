9613b10b8dec0f1fb7e08c99d5f2191305902b3a
Alan Somers <asomers@gmail.com>
2016-09-08 20:04:44 -0600

    Disable sys/event tests on Linux

--- test/sys/mod.rs
+++ test/sys/mod.rs
@@ -1,3 +1,5 @@
+#[cfg(any(target_os = "macos", target_os = "ios", target_os = "freebsd",
+    target_os = "dragonfly", target_os = "openbsd", target_os = "netbsd"))]
 mod test_event;
 mod test_socket;
 mod test_sockopt;
