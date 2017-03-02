7fa46987b5403d56e90a2a242511902f4b5f3f63
Zac Berkowitz <zac.berkowitz@gmail.com>
2017-03-02 18:01:58 +0000

    Fixed constants for MIPS

--- src/sys/socket/consts.rs
+++ src/sys/socket/consts.rs
@@ -11,14 +11,14 @@ mod os {
     pub const AF_NETLINK: c_int = 16;
     pub const AF_PACKET: c_int = 17;
 
-    pub const SOCK_STREAM: c_int = 1;
-    pub const SOCK_DGRAM: c_int = 2;
-    pub const SOCK_SEQPACKET: c_int = 5;
-    pub const SOCK_RAW: c_int = 3;
+    pub const SOCK_STREAM: c_int = libc::SOCK_STREAM;
+    pub const SOCK_DGRAM: c_int = libc::SOCK_DGRAM;
+    pub const SOCK_SEQPACKET: c_int = libc::SOCK_SEQPACKET;
+    pub const SOCK_RAW: c_int = libc::SOCK_RAW;
     pub const SOCK_RDM: c_int = 4;
 
     pub const SOL_IP: c_int     = 0;
-    pub const SOL_SOCKET: c_int = 1;
+    pub const SOL_SOCKET: c_int = libc::SOL_SOCKET;
     pub const SOL_TCP: c_int    = 6;
     pub const SOL_UDP: c_int    = 17;
     pub const SOL_IPV6: c_int   = 41;
@@ -28,36 +28,36 @@ mod os {
     pub const IPPROTO_TCP: c_int = SOL_TCP;
     pub const IPPROTO_UDP: c_int = SOL_UDP;
 
-    pub const SO_ACCEPTCONN: c_int = 30;
+    pub const SO_ACCEPTCONN: c_int = libc::SO_ACCEPTCONN;
     pub const SO_BINDTODEVICE: c_int = 25;
-    pub const SO_BROADCAST: c_int = 6;
+    pub const SO_BROADCAST: c_int = libc::SO_BROADCAST;
     pub const SO_BSDCOMPAT: c_int = 14;
     pub const SO_DEBUG: c_int = 1;
     pub const SO_DOMAIN: c_int = 39;
-    pub const SO_ERROR: c_int = 4;
-    pub const SO_DONTROUTE: c_int = 5;
-    pub const SO_KEEPALIVE: c_int = 9;
-    pub const SO_LINGER: c_int = 13;
+    pub const SO_ERROR: c_int = libc::SO_ERROR;
+    pub const SO_DONTROUTE: c_int = libc::SO_DONTROUTE;
+    pub const SO_KEEPALIVE: c_int = libc::SO_KEEPALIVE;
+    pub const SO_LINGER: c_int = libc::SO_LINGER;
     pub const SO_MARK: c_int = 36;
-    pub const SO_OOBINLINE: c_int = 10;
-    pub const SO_PASSCRED: c_int = 16;
+    pub const SO_OOBINLINE: c_int = libc::SO_OOBINLINE;
+    pub const SO_PASSCRED: c_int = libc::SO_PASSCRED;
     pub const SO_PEEK_OFF: c_int = 42;
-    pub const SO_PEERCRED: c_int = 17;
+    pub const SO_PEERCRED: c_int = libc::SO_PEERCRED;
     pub const SO_PRIORITY: c_int = 12;
     pub const SO_PROTOCOL: c_int = 38;
-    pub const SO_RCVBUF: c_int = 8;
+    pub const SO_RCVBUF: c_int = libc::SO_RCVBUF;
     pub const SO_RCVBUFFORCE: c_int = 33;
-    pub const SO_RCVLOWAT: c_int = 18;
-    pub const SO_SNDLOWAT: c_int = 19;
-    pub const SO_RCVTIMEO: c_int = 20;
-    pub const SO_SNDTIMEO: c_int = 21;
-    pub const SO_REUSEADDR: c_int = 2;
-    pub const SO_REUSEPORT: c_int = 15;
+    pub const SO_RCVLOWAT: c_int = libc::SO_RCVLOWAT;
+    pub const SO_SNDLOWAT: c_int = libc::SO_SNDLOWAT;
+    pub const SO_RCVTIMEO: c_int = libc::SO_RCVTIMEO;
+    pub const SO_SNDTIMEO: c_int = libc::SO_SNDTIMEO;
+    pub const SO_REUSEADDR: c_int = libc::SO_REUSEADDR;
+    pub const SO_REUSEPORT: c_int = libc::SO_REUSEPORT;
     pub const SO_RXQ_OVFL: c_int = 40;
-    pub const SO_SNDBUF: c_int = 7;
-    pub const SO_SNDBUFFORCE: c_int = 32;
+    pub const SO_SNDBUF: c_int = libc::SO_SNDBUF;
+    pub const SO_SNDBUFFORCE: c_int = libc::SO_SNDBUFFORCE;
     pub const SO_TIMESTAMP: c_int = 29;
-    pub const SO_TYPE: c_int = 3;
+    pub const SO_TYPE: c_int = libc::SO_TYPE;
     pub const SO_BUSY_POLL: c_int = 46;
     #[cfg(target_os = "linux")]
     pub const SO_ORIGINAL_DST: c_int = 80;
@@ -152,7 +152,7 @@ mod os {
     #[cfg(any(target_os = "macos", target_os = "ios"))]
     pub const SYSPROTO_CONTROL: c_int = 2;
 
-    pub const SO_ACCEPTCONN: c_int          = 0x0002;
+    pub const SO_ACCEPTCONN: c_int          = libc::SO_ACCEPTCONN;
     pub const SO_BROADCAST: c_int           = 0x0020;
     pub const SO_DEBUG: c_int               = 0x0001;
     #[cfg(not(target_os = "netbsd"))]
