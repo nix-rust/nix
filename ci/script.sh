ef9ef4f0c94c1bd64b5bd805d9041d871618adcc
Alan Somers <asomers@gmail.com>
2017-06-28 16:22:34 -0600

    Try using Travis' older images, pre 21-June

--- ci/script.sh
+++ ci/script.sh
@@ -9,7 +9,7 @@ main() {
     fi
 
     # Build debug and release targets
-    rustup default stable-$TARGET
+    #rustup default stable-$TARGET
     cross build --target $TARGET
     cross build --target $TARGET --release
 
