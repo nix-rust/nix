d3b0d083d1909bd34a89cc03a82eff143f27086f
Michael Neumann <mneumann@ntecs.de>
2017-07-17 22:37:16 +0200

    Fix errno on DragonFly
    Function __dfly_error() is gone from Rust since a long time. Rust's libstd uses
    the thread_local attribute for errno, but thread_local cannot be used
    from stable Rust. For this reason I created errno_dragonfly which is
    a C FFI wrapper for errno.

--- src/lib.rs
+++ src/lib.rs
@@ -21,6 +21,9 @@ extern crate void;
 #[cfg(test)]
 extern crate nix_test as nixtest;
 
+#[cfg(target_os = "dragonfly")]
+extern crate errno_dragonfly;
+
 #[macro_use] mod macros;
 
 // In rust 1.8+ this should be `pub extern crate libc` but prior
