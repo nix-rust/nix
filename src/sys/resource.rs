c10d071bc5e4af1fb9e583822be1233253166586
kpcyrd <git@rxv.cc>
2018-04-01 21:46:12 +0200

    Update API

--- src/sys/resource.rs
+++ src/sys/resource.rs
@@ -1,13 +1,7 @@
 use std::mem;
 
-use libc::{self, c_int};
-pub use libc::{rlimit, rlim_t, RLIM_INFINITY};
-
-#[cfg(any(target_os = "linux",
-          target_os = "openbsd",
-          target_os = "netbsd",
-          target_os = "bitrig"))]
-pub use libc::{RLIM_SAVED_CUR, RLIM_SAVED_MAX};
+use libc::{self, c_int, rlimit, RLIM_INFINITY};
+pub use libc::rlim_t;
 
 use {Errno, Result};
 
@@ -15,13 +9,14 @@ use {Errno, Result};
 #[repr(i32)]
 pub enum Resource {
     // POSIX
+    RLIMIT_AS = libc::RLIMIT_AS,
     RLIMIT_CORE = libc::RLIMIT_CORE,
     RLIMIT_CPU = libc::RLIMIT_CPU,
     RLIMIT_DATA = libc::RLIMIT_DATA,
     RLIMIT_FSIZE = libc::RLIMIT_FSIZE,
     RLIMIT_NOFILE = libc::RLIMIT_NOFILE,
     RLIMIT_STACK = libc::RLIMIT_STACK,
-    RLIMIT_AS = libc::RLIMIT_AS,
+
     // BSDs and Linux
     #[cfg(all(unix, not(target_os = "solaris")))]
     RLIMIT_MEMLOCK = libc::RLIMIT_MEMLOCK,
@@ -29,40 +24,35 @@ pub enum Resource {
     RLIMIT_NPROC = libc::RLIMIT_NPROC,
     #[cfg(all(unix, not(target_os = "solaris")))]
     RLIMIT_RSS = libc::RLIMIT_RSS,
-    // Linux-only
-    #[cfg(any(target_os = "linux", target_os = "android"))]
-    RLIMIT_LOCKS = libc::RLIMIT_LOCKS,
-    #[cfg(any(target_os = "linux", target_os = "android"))]
-    RLIMIT_MSGQUEUE = libc::RLIMIT_MSGQUEUE,
-    #[cfg(any(target_os = "linux", target_os = "android"))]
-    RLIMIT_NICE = libc::RLIMIT_NICE,
-    #[cfg(any(target_os = "linux", target_os = "android"))]
-    RLIMIT_RTPRIO = libc::RLIMIT_RTPRIO,
-    #[cfg(any(target_os = "linux", target_os = "android"))]
-    RLIMIT_RTTIME = libc::RLIMIT_RTTIME,
-    #[cfg(any(target_os = "linux", target_os = "android"))]
-    RLIMIT_SIGPENDING = libc::RLIMIT_SIGPENDING,
-}
 
-#[inline]
-fn rlim_to_option(rlim: rlim_t) -> Option<rlim_t> {
-    match rlim {
-        RLIM_INFINITY => None,
-        rlim => Some(rlim),
-    }
+    // Android and Linux only
+    #[cfg(any(target_os = "android", target_os = "linux"))]
+    RLIMIT_LOCKS = libc::RLIMIT_LOCKS,
+    #[cfg(any(target_os = "android", target_os = "linux"))]
+    RLIMIT_MSGQUEUE = libc::RLIMIT_MSGQUEUE,
+    #[cfg(any(target_os = "android", target_os = "linux"))]
+    RLIMIT_NICE = libc::RLIMIT_NICE,
+    #[cfg(any(target_os = "android", target_os = "linux"))]
+    RLIMIT_RTPRIO = libc::RLIMIT_RTPRIO,
+    #[cfg(any(target_os = "android", target_os = "linux"))]
+    RLIMIT_RTTIME = libc::RLIMIT_RTTIME,
+    #[cfg(any(target_os = "android", target_os = "linux"))]
+    RLIMIT_SIGPENDING = libc::RLIMIT_SIGPENDING,
 }
 
 pub fn getrlimit(resource: Resource) -> Result<(Option<rlim_t>, Option<rlim_t>)> {
     let mut rlim: rlimit = unsafe { mem::uninitialized() };
     let res = unsafe { libc::getrlimit(resource as c_int, &mut rlim as *mut _) };
-    Errno::result(res)?;
-    Ok((rlim_to_option(rlim.rlim_cur), rlim_to_option(rlim.rlim_max)))
+    Errno::result(res).map(|_| {
+        (if rlim.rlim_cur != RLIM_INFINITY { Some(rlim.rlim_cur) } else { None },
+         if rlim.rlim_max != RLIM_INFINITY { Some(rlim.rlim_max) } else { None })
+    })
 }
 
-pub fn setrlimit(resource: Resource, limit: (Option<rlim_t>, Option<rlim_t>)) -> Result<()> {
+pub fn setrlimit(resource: Resource, soft_limit: Option<rlim_t>, hard_limit: Option<rlim_t>) -> Result<()> {
     let mut rlim: rlimit = unsafe { mem::uninitialized() };
-    rlim.rlim_cur = limit.0.unwrap_or(RLIM_INFINITY);
-    rlim.rlim_max = limit.1.unwrap_or(RLIM_INFINITY);
+    rlim.rlim_cur = soft_limit.unwrap_or(RLIM_INFINITY);
+    rlim.rlim_max = hard_limit.unwrap_or(RLIM_INFINITY);
 
     let res = unsafe { libc::setrlimit(resource as c_int, &rlim as *const _) };
     Errno::result(res).map(drop)
--- test/test_resource.rs
+++ test/test_resource.rs
@@ -2,19 +2,17 @@ use nix::sys::resource::{Resource, getrlimit, setrlimit};
 
 #[test]
 pub fn test_resource_limits() {
-    let mut limit = getrlimit(Resource::RLIMIT_STACK).unwrap();
-    assert!(limit.0 != limit.1);
+    let (mut soft_limit, hard_limit) = getrlimit(Resource::RLIMIT_STACK).unwrap();
+    let orig_limit = (soft_limit, hard_limit);
 
-    let orig_limit = limit;
-
-    limit.0 = limit.1;
-    setrlimit(Resource::RLIMIT_STACK, limit).unwrap();
+    soft_limit = hard_limit;
+    setrlimit(Resource::RLIMIT_STACK, soft_limit, hard_limit).unwrap();
 
     let limit2 = getrlimit(Resource::RLIMIT_STACK).unwrap();
-    assert_eq!(limit.0, limit2.0);
-    assert_eq!(limit.1, limit2.1);
+    assert_eq!(soft_limit, limit2.0);
+    assert_eq!(hard_limit, limit2.1);
 
-    setrlimit(Resource::RLIMIT_STACK, orig_limit).unwrap();
+    setrlimit(Resource::RLIMIT_STACK, orig_limit.0, orig_limit.1).unwrap();
 
     let final_limit = getrlimit(Resource::RLIMIT_STACK).unwrap();
     assert_eq!(orig_limit.0, final_limit.0);
