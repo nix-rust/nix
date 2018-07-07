0e1a7da4ae4835b087d7d0c177ca6a6146bed2b0
Curtis Millar <curtis@curtism.me>
2018-07-07 12:35:53 +1000

    Revert "Mask clone test."
    This reverts commit e78c659fc20de796d59fc224bb817a8d5e369014.

--- src/sched.rs
+++ src/sched.rs
@@ -120,7 +120,7 @@ pub fn setns(fd: RawFd, nstype: CloneFlags) -> Result<()> {
     Errno::result(res).map(drop)
 }
 
-#[cfg(not)]
+#[cfg(test)]
 mod test {
     use super::*;
     use sys::wait::{waitpid, WaitStatus};
