8aa32c83d9fb8290ff777602e84bd56c8040d2f8
Alan Somers <asomers@gmail.com>
2019-06-03 07:22:27 -0600

    Fix an "unused use statement" warning on non-musl platforms
    This was an oversight from 974320d09fbd1fae79da537383c2144ed9aa4967

--- test/sys/test_pthread.rs
+++ test/sys/test_pthread.rs
@@ -1,11 +1,10 @@
 use nix::sys::pthread::*;
-use std::ptr;
 
 #[cfg(target_env = "musl")]
 #[test]
 fn test_pthread_self() {
     let tid = pthread_self();
-    assert!(tid != ptr::null_mut());
+    assert!(tid != ::std::ptr::null_mut());
 }
 
 #[cfg(not(target_env = "musl"))]
