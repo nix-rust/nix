974320d09fbd1fae79da537383c2144ed9aa4967
Alan Somers <asomers@gmail.com>
2019-06-02 21:04:48 -0600

    Fix test_pthread_self on musl
    On musl, pthread_t is a pointer type.

--- test/sys/test_pthread.rs
+++ test/sys/test_pthread.rs
@@ -1,5 +1,14 @@
 use nix::sys::pthread::*;
+use std::ptr;
 
+#[cfg(target_env = "musl")]
+#[test]
+fn test_pthread_self() {
+    let tid = pthread_self();
+    assert!(tid != ptr::null_mut());
+}
+
+#[cfg(not(target_env = "musl"))]
 #[test]
 fn test_pthread_self() {
     let tid = pthread_self();
