4db701dafedf6dacce465bb90d4594628b67dbf8
Vitaly _Vi Shukela <vi0oss@gmail.com>
2020-09-12 12:36:27 +0300

    Adapt for safe-isation of some libc functions.
    (https://github.com/rust-lang/libc/pull/1870)
    
    Denied lint being overridden instead of unsafe block removed
    to preserve compatibility with old `libc` versions.

--- src/sys/wait.rs
+++ src/sys/wait.rs
@@ -117,35 +117,43 @@ impl WaitStatus {
     }
 }
 
+#[allow(unused_unsafe)]
 fn exited(status: i32) -> bool {
     unsafe { libc::WIFEXITED(status) }
 }
 
+#[allow(unused_unsafe)]
 fn exit_status(status: i32) -> i32 {
     unsafe { libc::WEXITSTATUS(status) }
 }
 
+#[allow(unused_unsafe)]
 fn signaled(status: i32) -> bool {
     unsafe { libc::WIFSIGNALED(status) }
 }
 
+#[allow(unused_unsafe)]
 fn term_signal(status: i32) -> Result<Signal> {
     Signal::try_from(unsafe { libc::WTERMSIG(status) })
 }
 
+#[allow(unused_unsafe)]
 fn dumped_core(status: i32) -> bool {
     unsafe { libc::WCOREDUMP(status) }
 }
 
+#[allow(unused_unsafe)]
 fn stopped(status: i32) -> bool {
     unsafe { libc::WIFSTOPPED(status) }
 }
 
+#[allow(unused_unsafe)]
 fn stop_signal(status: i32) -> Result<Signal> {
     Signal::try_from(unsafe { libc::WSTOPSIG(status) })
 }
 
 #[cfg(any(target_os = "android", target_os = "linux"))]
+#[allow(unused_unsafe)]
 fn syscall_stop(status: i32) -> bool {
     // From ptrace(2), setting PTRACE_O_TRACESYSGOOD has the effect
     // of delivering SIGTRAP | 0x80 as the signal number for syscall
@@ -159,6 +167,7 @@ fn stop_additional(status: i32) -> c_int {
     (status >> 16) as c_int
 }
 
+#[allow(unused_unsafe)]
 fn continued(status: i32) -> bool {
     unsafe { libc::WIFCONTINUED(status) }
 }
