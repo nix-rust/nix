6d9ebf271cec48c4715157f1a69d44aa15ecf110
Alex Rawson <ajzecrom@gmail.com>
2022-05-31 17:51:45 -0500

    Ignore doctests for unexported macros
    Due to rust-lang/rust#97030, cargo test will fail to doctest macros
    unless they are exported, breaking the examples for libc_bitflags! and
    libc_enum!.
    
    Adds `ignore` to the examples for these macros to stop tests from
    failing.

--- src/macros.rs
+++ src/macros.rs
@@ -5,7 +5,7 @@
 /// The `libc` crate must be in scope with the name `libc`.
 ///
 /// # Example
-/// ```
+/// ```ignore
 /// libc_bitflags!{
 ///     pub struct ProtFlags: libc::c_int {
 ///         PROT_NONE;
@@ -25,7 +25,7 @@
 /// various flags have different types, so we cast the broken ones to the right
 /// type.
 ///
-/// ```
+/// ```ignore
 /// libc_bitflags!{
 ///     pub struct SaFlags: libc::c_ulong {
 ///         SA_NOCLDSTOP as libc::c_ulong;
@@ -66,7 +66,7 @@ macro_rules! libc_bitflags {
 /// The `libc` crate must be in scope with the name `libc`.
 ///
 /// # Example
-/// ```
+/// ```ignore
 /// libc_enum!{
 ///     pub enum ProtFlags {
 ///         PROT_NONE,
