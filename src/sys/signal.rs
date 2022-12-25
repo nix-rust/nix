2e0f533079ec479ff62abb42f275924a14eb037f
Xiaobo Liu <cppcoffee@gmail.com>
2022-12-25 10:07:17 +0800

    fix: linux+mips+uclibc unreachable pattern
    Signed-off-by: Xiaobo Liu <cppcoffee@gmail.com>

--- src/sys/signal.rs
+++ src/sys/signal.rs
@@ -1066,7 +1066,7 @@ mod sigevent {
                 SigevNotify::SigevThreadId{..} => libc::SIGEV_THREAD_ID,
                 #[cfg(all(target_os = "linux", target_env = "gnu", not(target_arch = "mips")))]
                 SigevNotify::SigevThreadId{..} => libc::SIGEV_THREAD_ID,
-                #[cfg(all(target_os = "linux", target_env = "uclibc"))]
+                #[cfg(all(target_os = "linux", target_env = "uclibc", not(target_arch = "mips")))]
                 SigevNotify::SigevThreadId{..} => libc::SIGEV_THREAD_ID,
                 #[cfg(any(all(target_os = "linux", target_env = "musl"), target_arch = "mips"))]
                 SigevNotify::SigevThreadId{..} => 4  // No SIGEV_THREAD_ID defined
