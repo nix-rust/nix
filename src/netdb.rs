387b731dee52b15d390c78b361f8c977f42694cc
Dante-Broggi <34220985+Dante-Broggi@users.noreply.github.com>
2023-03-14 16:10:55 -0400

    integer type rexports

--- src/netdb.rs
+++ src/netdb.rs
@@ -2,3 +2,7 @@
 //! 
 //! https://pubs.opengroup.org/onlinepubs/9699919799/basedefs/netdb.h.html
 
+// The <netdb.h> header may define the in_port_t type and the in_addr_t type as described in <netinet/in.h>.
+// Simple integer type aliases, so we rexport
+pub use libc::{in_port_t, in_addr_t};
+pub use libc::socklen_t;
