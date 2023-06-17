e63dd8fa5944e945557c1605b633345d07aeea27
Niels Sascha Reedijk <niels.reedijk@gmail.com>
2023-06-17 15:04:46 +0000

    Haiku: `speed_t` is defined as `u8` for 32 and 64 bit systems

--- src/sys/termios.rs
+++ src/sys/termios.rs
@@ -355,9 +355,9 @@ libc_enum! {
     /// enum.
     ///
     /// B0 is special and will disable the port.
-    #[cfg_attr(all(any(target_os = "haiku"), target_pointer_width = "64"), repr(u8))]
+    #[cfg_attr(target_os = "haiku", repr(u8))]
     #[cfg_attr(all(any(target_os = "ios", target_os = "macos"), target_pointer_width = "64"), repr(u64))]
-    #[cfg_attr(not(all(any(target_os = "ios", target_os = "macos", target_os = "haiku"), target_pointer_width = "64")), repr(u32))]
+    #[cfg_attr(all(not(all(any(target_os = "ios", target_os = "macos"), target_pointer_width = "64")), not(target_os = "haiku")), repr(u32))]
     #[non_exhaustive]
     pub enum BaudRate {
         B0,
