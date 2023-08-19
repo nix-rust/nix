b6572edf5d2e8c55e063873eeac491e7674fd53d
Christopher Skane <sokrbball@gmail.com>
2023-08-19 13:43:28 -0400

    Removed redundant  cfg condition.

--- src/mqueue.rs
+++ src/mqueue.rs
@@ -37,9 +37,10 @@ use crate::sys::stat::Mode;
 use libc::{self, c_char, mqd_t, size_t};
 use std::ffi::CStr;
 use std::mem;
-#[cfg(all(
-    unix,
-    any(target_os = "linux", target_os = "netbsd", target_os = "dragonfly")
+#[cfg(any(
+    target_os = "linux",
+    target_os = "netbsd",
+    target_os = "dragonfly"
 ))]
 use std::os::unix::io::{
     AsFd, AsRawFd, BorrowedFd, FromRawFd, IntoRawFd, RawFd,
@@ -308,10 +309,7 @@ pub fn mq_remove_nonblock(mqd: &MqdT) -> Result<MqAttr> {
     mq_setattr(mqd, &newattr)
 }
 
-#[cfg(all(
-    unix,
-    any(target_os = "linux", target_os = "netbsd", target_os = "dragonfly")
-))]
+#[cfg(any(target_os = "linux", target_os = "netbsd", target_os = "dragonfly"))]
 impl AsFd for MqdT {
     /// Borrow the underlying message queue descriptor.
     fn as_fd(&self) -> BorrowedFd {
@@ -320,10 +318,7 @@ impl AsFd for MqdT {
     }
 }
 
-#[cfg(all(
-    unix,
-    any(target_os = "linux", target_os = "netbsd", target_os = "dragonfly")
-))]
+#[cfg(any(target_os = "linux", target_os = "netbsd", target_os = "dragonfly"))]
 impl AsRawFd for MqdT {
     /// Return the underlying message queue descriptor.
     ///
@@ -334,10 +329,7 @@ impl AsRawFd for MqdT {
     }
 }
 
-#[cfg(all(
-    unix,
-    any(target_os = "linux", target_os = "netbsd", target_os = "dragonfly")
-))]
+#[cfg(any(target_os = "linux", target_os = "netbsd", target_os = "dragonfly"))]
 impl FromRawFd for MqdT {
     /// Construct an [MqdT] from [RawFd].
     ///
@@ -349,10 +341,7 @@ impl FromRawFd for MqdT {
     }
 }
 
-#[cfg(all(
-    unix,
-    any(target_os = "linux", target_os = "netbsd", target_os = "dragonfly")
-))]
+#[cfg(any(target_os = "linux", target_os = "netbsd", target_os = "dragonfly"))]
 impl IntoRawFd for MqdT {
     /// Consume this [MqdT] and return a [RawFd].
     fn into_raw_fd(self) -> RawFd {
