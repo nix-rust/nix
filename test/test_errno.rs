c1114de440f0e384c956f3bae51994c3ce7b77a7
PolyMeilex <marynczakbartlomiej@gmail.com>
2024-01-07 16:13:25 +0100

    errno_set_and_clear test

--- test/test_errno.rs
+++ test/test_errno.rs
@@ -6,3 +6,12 @@ fn errno_set_and_read() {
     Errno::ENFILE.set();
     assert_eq!(Errno::last(), Errno::ENFILE);
 }
+
+#[test]
+fn errno_set_and_clear() {
+    Errno::ENFILE.set();
+    assert_eq!(Errno::last(), Errno::ENFILE);
+
+    Errno::clear();
+    assert_eq!(Errno::last(), Errno::from_raw(0));
+}
