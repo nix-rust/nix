fee5acb3ea5ec142a2450be26e76f0492095de2b
IzawGithub <125356732+IzawGithub@users.noreply.github.com>
2024-03-25 20:40:06 +0100

    Formatting parse

--- test/sys/mod.rs
+++ test/sys/mod.rs
@@ -25,13 +25,7 @@ mod test_mman;
 mod test_select;
 #[cfg(target_os = "linux")]
 mod test_signalfd;
-#[cfg(all(
-    any(
-        bsd,
-        target_os = "linux",
-    ),
-    feature = "sysvipc",
-))]
+#[cfg(all(any(bsd, target_os = "linux",), feature = "sysvipc",))]
 mod test_smh;
 #[cfg(not(any(target_os = "redox", target_os = "haiku")))]
 mod test_socket;
