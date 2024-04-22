f7c327c2d017658a017e6f1f4cfd0647e48b4ef6
Jonathan Krebs <jonny@git2024.olpp.net>
2024-04-22 23:59:00 +0200

    tests: clarify the meaning of the FORK_MTX

--- test/test.rs
+++ test/test.rs
@@ -58,7 +58,8 @@ fn read_exact<Fd: AsFd>(f: Fd, buf: &mut [u8]) {
 }
 
 /// Any test that creates child processes must grab this mutex, regardless
-/// of what it does with those children.
+/// of what it does with those children. It must hold the mutex until the
+/// child processes are waited upon.
 pub static FORK_MTX: Mutex<()> = Mutex::new(());
 /// Any test that changes the process's current working directory must grab
 /// the RwLock exclusively.  Any process that cares about the current
