8eea05b88bda016a905caf358fa3882f757b95ed
Steve Lau <stevelauc@outlook.com>
2024-05-22 21:01:01 +0800

    revert: bring the test in assert_fs_equals_strict back as it is ignored

--- test/sys/test_statfs.rs
+++ test/sys/test_statfs.rs
@@ -92,6 +92,7 @@ fn assert_fs_equals_strict(fs: Statfs, vfs: Statvfs) {
     assert_eq!(fs.files_free() as u64, vfs.files_free() as u64);
     assert_eq!(fs.blocks_free() as u64, vfs.blocks_free() as u64);
     assert_eq!(fs.blocks_available() as u64, vfs.blocks_available() as u64);
+    assert_eq!(fs.files() as u64, vfs.files() as u64);
     assert_eq!(fs.blocks() as u64, vfs.blocks() as u64);
     assert_eq!(fs.block_size() as u64, vfs.fragment_size() as u64);
 }
