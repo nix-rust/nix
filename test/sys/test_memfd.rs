57f866f3a2c021908eef6662534d249bfbafa484
Steve Lau <stevelauc@outlook.com>
2024-06-08 16:05:43 +0800

    test: move imports to function body

--- test/sys/test_memfd.rs
+++ test/sys/test_memfd.rs
@@ -1,14 +1,14 @@
-use nix::sys::memfd::memfd_create;
-use nix::sys::memfd::MemFdCreateFlag;
-use nix::unistd::lseek;
-use nix::unistd::read;
-use nix::unistd::{write, Whence};
-use std::os::fd::{AsFd, AsRawFd};
-
 #[test]
 // Looks like symbol `memfd_create()` is unavailable: https://github.com/nix-rust/nix/actions/runs/9427112650/job/25970870477
 #[cfg_attr(qemu, ignore)]
 fn test_memfd_create() {
+    use nix::sys::memfd::memfd_create;
+    use nix::sys::memfd::MemFdCreateFlag;
+    use nix::unistd::lseek;
+    use nix::unistd::read;
+    use nix::unistd::{write, Whence};
+    use std::os::fd::{AsFd, AsRawFd};
+
     let fd =
         memfd_create("test_memfd_create_name", MemFdCreateFlag::MFD_CLOEXEC)
             .unwrap();
