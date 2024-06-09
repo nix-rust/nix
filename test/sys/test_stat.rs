a3fd1b7d4ef7779e18452cb897a66e6f7efcaefe
Steve Lau <stevelauc@outlook.com>
2024-06-09 18:32:31 +0800

    fix test on FreeBSD & correct changelog PR number

--- test/sys/test_stat.rs
+++ test/sys/test_stat.rs
@@ -488,23 +488,20 @@ fn test_chflags() {
         sys::stat::{fstat, FileFlag},
         unistd::chflags,
     };
-    use std::os::unix::io::AsRawFd;
     use tempfile::NamedTempFile;
 
     let f = NamedTempFile::new().unwrap();
 
-    let initial = FileFlag::from_bits_truncate(
-        fstat(f.as_raw_fd()).unwrap().st_flags.into(),
-    );
+    let initial =
+        FileFlag::from_bits_truncate(fstat(&f).unwrap().st_flags.into());
     // UF_OFFLINE is preserved by all FreeBSD file systems, but not interpreted
     // in any way, so it's handy for testing.
     let commanded = initial ^ FileFlag::UF_OFFLINE;
 
     chflags(f.path(), commanded).unwrap();
 
-    let changed = FileFlag::from_bits_truncate(
-        fstat(f.as_raw_fd()).unwrap().st_flags.into(),
-    );
+    let changed =
+        FileFlag::from_bits_truncate(fstat(&f).unwrap().st_flags.into());
 
     assert_eq!(commanded, changed);
 }
