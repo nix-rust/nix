ca11b517badb047a43fd46789c4f382525e283c5
David Carlier <devnexen@gmail.com>
2024-12-11 12:28:10 +0000

    try testing MmapAdvise::MADV_WIPEONFORK

--- test/sys/test_mman.rs
+++ test/sys/test_mman.rs
@@ -156,3 +156,36 @@ fn test_mremap_dontunmap() {
         std::slice::from_raw_parts_mut(mem.cast().as_ptr(), 10 * ONE_K)
     };
 }
+
+#[test]
+#[cfg(target_os = "linux")]
+fn test_madv_wipeonfork() {
+    use nix::libc::size_t;
+    use nix::sys::mman::{madvise, MmapAdvise};
+    use nix::unistd::{fork, ForkResult};
+    use std::num::NonZeroUsize;
+
+    const ONE_K: size_t = 1024;
+    let ten_one_k = NonZeroUsize::new(10 * ONE_K).unwrap();
+    let slice: &mut [u8] = unsafe {
+        let mem = mmap_anonymous(
+            None,
+            ten_one_k,
+            ProtFlags::PROT_READ | ProtFlags::PROT_WRITE,
+            MapFlags::MAP_PRIVATE,
+        )
+        .unwrap();
+        madvise(mem, ONE_K, MmapAdvise::MADV_WIPEONFORK)
+            .expect("madvise failed");
+        std::slice::from_raw_parts_mut(mem.as_ptr().cast(), ONE_K)
+    };
+    slice[ONE_K - 1] = 0xFF;
+    let _m = crate::FORK_MTX.lock();
+
+    unsafe {
+        if let ForkResult::Child = fork().expect("fork failed") {
+            // that s the while point of MADV_WIPEONFORK
+            assert_eq!(slice[ONE_K - 1], 0x00);
+        }
+    }
+}
