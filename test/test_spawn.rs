f6ae010e02c5fcda55424f405612d42e7d0a3cec
Steve Lau <stevelauc@outlook.com>
2025-02-02 22:34:12 +0800

    test: test posix_spawn()

--- test/test_spawn.rs
+++ test/test_spawn.rs
@@ -8,6 +8,85 @@ use std::ffi::CString;
 fn spawn_true() {
     let _guard = FORK_MTX.lock();
 
+    let bin = which::which("true").unwrap();
+    let args = &[
+        CString::new("true").unwrap(),
+        CString::new("story").unwrap(),
+    ];
+    let vars: &[CString] = &[];
+    let actions = PosixSpawnFileActions::init().unwrap();
+    let attr = PosixSpawnAttr::init().unwrap();
+
+    let pid =
+        spawn::posix_spawn(bin.as_path(), &actions, &attr, args, vars).unwrap();
+
+    let status = waitpid(pid, Some(WaitPidFlag::empty())).unwrap();
+
+    match status {
+        WaitStatus::Exited(wpid, ret) => {
+            assert_eq!(pid, wpid);
+            assert_eq!(ret, 0);
+        }
+        _ => {
+            panic!("Invalid WaitStatus");
+        }
+    };
+}
+
+#[test]
+fn spawn_sleep() {
+    let _guard = FORK_MTX.lock();
+
+    let bin = which::which("sleep").unwrap();
+    let args = &[CString::new("sleep").unwrap(), CString::new("30").unwrap()];
+    let vars: &[CString] = &[];
+    let actions = PosixSpawnFileActions::init().unwrap();
+    let attr = PosixSpawnAttr::init().unwrap();
+
+    let pid =
+        spawn::posix_spawn(bin.as_path(), &actions, &attr, args, vars).unwrap();
+
+    let status =
+        waitpid(pid, WaitPidFlag::from_bits(WaitPidFlag::WNOHANG.bits()))
+            .unwrap();
+    match status {
+        WaitStatus::StillAlive => {}
+        _ => {
+            panic!("Invalid WaitStatus");
+        }
+    };
+
+    signal::kill(pid, signal::SIGTERM).unwrap();
+
+    let status = waitpid(pid, Some(WaitPidFlag::empty())).unwrap();
+    match status {
+        WaitStatus::Signaled(wpid, wsignal, _) => {
+            assert_eq!(pid, wpid);
+            assert_eq!(wsignal, signal::SIGTERM);
+        }
+        _ => {
+            panic!("Invalid WaitStatus");
+        }
+    };
+}
+
+#[test]
+fn spawn_cmd_does_not_exist() {
+    let _guard = FORK_MTX.lock();
+
+    let args = &[CString::new("buzz").unwrap()];
+    let envs: &[CString] = &[];
+    let actions = PosixSpawnFileActions::init().unwrap();
+    let attr = PosixSpawnAttr::init().unwrap();
+
+    let bin = "2b7433c4-523b-470c-abb5-d7ee9fd295d5-fdasf";
+    let pid = spawn::posix_spawn(bin, &actions, &attr, args, envs).unwrap();
+}
+
+#[test]
+fn spawnp_true() {
+    let _guard = FORK_MTX.lock();
+
     let bin = &CString::new("true").unwrap();
     let args = &[
         CString::new("true").unwrap(),
@@ -33,7 +112,7 @@ fn spawn_true() {
 }
 
 #[test]
-fn spawn_sleep() {
+fn spawnp_sleep() {
     let _guard = FORK_MTX.lock();
 
     let bin = &CString::new("sleep").unwrap();
