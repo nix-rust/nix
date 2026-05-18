d1976c1fefee4750544f1dc21b9d22f5dd201a8e
Steve Lau <stevelauc@outlook.com>
2026-05-18 12:32:30 +0800

    address review comments

--- test/common/mod.rs
+++ test/common/mod.rs
@@ -142,26 +142,25 @@ cfg_if! {
     if #[cfg(linux_android)] {
         #[macro_export] macro_rules! require_kernel_module {
             ($name:expr, $module_name:expr) => {
-                let loaded = std::fs::read_to_string("/proc/modules")
+                let loaded_from_proc = std::fs::read_to_string("/proc/modules")
                     .map(|modules| {
                         modules.lines().any(|line| {
                             line.split_whitespace().next()
                                 == Some($module_name)
                         })
                     })
-                    .unwrap_or_else(|_| {
-                        let module_path =
-                            format!("/sys/module/{}", $module_name);
-                        match std::path::Path::new(&module_path).try_exists()
-                        {
-                            Ok(exists) => exists,
-                            Err(error) => {
-                                panic!(
-                                    "failed to inspect {module_path}: {error}"
-                                )
-                            }
+                    .unwrap_or(false);
+
+                let module_path = format!("/sys/module/{}", $module_name);
+                let loaded_from_sys =
+                    match std::path::Path::new(&module_path).try_exists() {
+                        Ok(exists) => exists,
+                        Err(error) => {
+                            panic!("failed to inspect {module_path}: {error}")
                         }
-                    });
+                    };
+
+                let loaded = loaded_from_proc || loaded_from_sys;
 
                 if !loaded {
                     $crate::skip!(
